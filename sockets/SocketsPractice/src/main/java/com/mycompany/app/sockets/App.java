a9202876bdeb435deb63cde5b94d45678ad98919
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-03-15 16:55:21 -0400

    Creacion del cliente DailyAdviceClient

--- sockets/SocketsPractice/src/main/java/com/mycompany/app/sockets/App.java
+++ sockets/SocketsPractice/src/main/java/com/mycompany/app/sockets/App.java
@@ -1,13 +1,11 @@
 package com.mycompany.app.sockets;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        DailyAdviceClient adviceClient = new DailyAdviceClient();
+        adviceClient.go();
     }
 }
---
+++ sockets/SocketsPractice/src/main/java/com/mycompany/app/sockets/DailyAdviceClient.java
@@ -0,0 +1,33 @@
+package com.mycompany.app.sockets;
+
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class DailyAdviceClient 
+{
+    public void go() {
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", 4242);
+            
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+            
+            String advice = reader.readLine();
+            System.out.println("Today you should: ".concat(advice));
+            
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}

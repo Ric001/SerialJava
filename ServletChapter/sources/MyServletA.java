3fc4cadf41cb629194093a6f069b5a04992f0669
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-04-04 16:45:18 -0400

    Servlet Project initialized

---
+++ ServletChapter/sources/MyServletA.java
@@ -0,0 +1,39 @@
+package sources;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value = "/example")
+public class MyServletA extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+            response.setContentType("text/html");
+
+            try (PrintWriter out = response.getWriter()) {
+                String message = "If you're reading this, it worked!";
+                
+                out.println("<html><body>");
+                out.println("<h1>".concat(message).concat("</h1>"));
+                out.println("</body></html>");
+                out.flush();
+            } 
+        }
+    
+    
+    @Override 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+        
+            response.setContentType("text/html");
+            
+            try (PrintWriter out = response.getWriter()){
+                
+
+                out.println("<html><body>ERROR</body></html>");
+                out.flush();
+            }
+    }
+}
